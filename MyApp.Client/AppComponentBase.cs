3c798dfdba70a3b07881dd6d312f149748b92a96
Darren Reid <darren@reidmail.org>
2023-11-22 19:15:19 +1100

    Revert "Sync gh actions"

--- MyApp.Client/AppComponentBase.cs
+++ MyApp.Client/AppComponentBase.cs
@@ -1,3 +1,8 @@
+using System;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Authorization;
 using ServiceStack;
 using ServiceStack.Blazor;
 
